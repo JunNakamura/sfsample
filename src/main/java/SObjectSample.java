57e22077bea1cc7d4391930b19a622e294310556
jun.nakamura <n.slenderness@gmail.com>
2017-02-07 15:40:34 +0900

    upsertのサンプルコードを追加

--- src/main/java/SObjectSample.java
+++ src/main/java/SObjectSample.java
@@ -4,12 +4,18 @@
 
 import com.sforce.soap.enterprise.EnterpriseConnection;
 import com.sforce.soap.enterprise.SaveResult;
+import com.sforce.soap.enterprise.UpsertResult;
 import com.sforce.soap.enterprise.sobject.Account;
 import com.sforce.soap.enterprise.sobject.SObject;
 import com.sforce.ws.ConnectionException;
+import java.time.LocalDate;
 import util.ConnectionUtil;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,17 +29,56 @@ public class SObjectSample {
 	
 	private static final Logger logger = LoggerFactory.getLogger(SObjectSample.class);
 
-    public static void main(String[] args) throws ConnectionException {
+    public static void main(String[] args) throws ConnectionException, InterruptedException {
         EnterpriseConnection connection = ConnectionUtil.createEPC();
 
-        Account account = new Account();
-        account.setName("00test-" + LocalDateTime.now());
-        SObject[] so = new SObject[]{account};
+        SObject[] so = new SObject[200];
+        List<Account> accounts = new ArrayList<>();
+        for (int i = 0; i < 200; i++) {
+            Account account = new Account();
+            account.setName("000test-upsert-" + i + "-" + LocalDate.now());
+            account.setExt_id__c(UUID.randomUUID().toString());
+            so[i] = account;
+            accounts.add(account);
+        }
+        
         SaveResult[] res = connection.create(so);
 
         for (SaveResult result: res) {
         	logger.info(result.toString());
         }
+        
+        //ランダム要素のためだけ
+        Random random = new Random();
+        
+        // 更新日時が分までなので差を出すために待機
+        //TimeUnit.MINUTES.sleep(1);
+        
+        List<Account> _accounts = new ArrayList<>();
+        
+        for (Account account: accounts) {
+        	account.setActive__c("yes");
+        	if (random.nextBoolean()) {
+        		//　ランダムで新規作成になるものを混ぜる
+        		account.setActive__c("no");
+        		account.setExt_id__c(UUID.randomUUID().toString());
+        	}
+        	_accounts.add(account);
+        }
+        
+        // 200件を超えるために追加
+        Account dummy = new Account();
+        dummy.setName("00-test-dummy");
+        dummy.setExt_id__c(UUID.randomUUID().toString());
+        _accounts.add(dummy);
+        
+        
+        UpsertResult[] res2 = connection.upsert("ext_id__c", _accounts.toArray(new Account[_accounts.size()]));
+        for (UpsertResult result: res2) {
+        	logger.info("upsert: {}", result);
+        }
+        
+        
 
     }
 
